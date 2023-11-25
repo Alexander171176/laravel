b07dddb499cf0ece6fab612a76c05646db28f095
Александр Косолапов <kosolapov1976@gmail.com>
2023-11-25 10:43:53 +0600

    fix:bootstrap and tailwind styles - table

--- resources/js/Pages/BlogPost/Index.vue
+++ resources/js/Pages/BlogPost/Index.vue
@@ -45,60 +45,60 @@ defineProps({
     <h1 class="mb-4 text-center font-semibold text-xl text-black dark:text-white">Список постов</h1>
     <div class="flex flex-col gap-10">
       <div
-        class="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1"
+        class="rounded-md border border-[#999] border-stroke bg-white pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1"
       >
         <div class="max-w-full overflow-x-auto">
           <table class="w-full table-auto">
-            <thead>
+            <thead class="bg-slate-500/[.06] border-b border-[#999]">
               <tr class="bg-gray-2 text-left dark:bg-meta-4">
-                <th class="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
+                <th class="min-w-[120px] py-4 font-medium text-black dark:text-white xl:pl-11">
                   ID
                 </th>
-                <th class="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
+                <th class="min-w-[120px] py-4 font-medium text-black dark:text-white xl:pl-11">
                   Название
                 </th>
-                <th class="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
+                <th class="min-w-[120px] py-4 font-medium text-black dark:text-white">
                   Ссылка
                 </th>
-                <th class="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
+                <th class="min-w-[120px] py-4 font-medium text-black dark:text-white">
                   Автор
                 </th>
-                <th class="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
+                <th class="min-w-[120px] py-4 font-medium text-black dark:text-white">
                   Изображение
                 </th>
-                <th class="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
+                <th class="min-w-[120px] py-4 font-medium text-black dark:text-white">
                   Описание
                 </th>
-                <th class="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
+                <th class="min-w-[120px] py-4 font-medium text-black dark:text-white">
                   Дата публикации
                 </th>
               </tr>
             </thead>
             <tbody>
               <tr v-for="post in posts" :key="post.id">
-                <td class="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                <td class="border-b border-[#999] dark:border-strokedark">
                   <p
-                    class="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success"
+                    class="inline-flex bg-opacity-10 py-1 px-3 text-sm font-medium"
                   >
                     {{ post.id }}
                   </p>
                 </td>
-                <td class="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
+                <td class="border-b border-[#999] pl-9 dark:border-strokedark xl:pl-11">
                   <h5 class="font-medium text-black dark:text-white">{{ post.title }}</h5>
                 </td>
-                <td class="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                <td class="border-b border-[#999] dark:border-strokedark">
                   <p class="text-black dark:text-white">{{ post.url }}</p>
                 </td>
-                <td class="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                <td class="border-b border-[#999] dark:border-strokedark">
                   <p
-                    class="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success"
+                    class="inline-flex bg-opacity-10 py-1 px-3 text-sm font-medium"
                   >
                     {{ post.author }}
                   </p>
                 </td>
-                <td class="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                <td class="border-b border-[#999] dark:border-strokedark">
                   <p
-                    class="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success"
+                    class="inline-flex bg-opacity-10 py-1 px-3 text-sm font-medium"
                   >
                     <img
                       v-if="post.image_url"
@@ -109,16 +109,16 @@ defineProps({
                     <span v-else>No Image</span>
                   </p>
                 </td>
-                <td class="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                <td class="border-b border-[#999] dark:border-strokedark">
                   <p
-                    class="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success"
+                    class="inline-flex bg-opacity-10 py-1 px-3 text-sm font-medium"
                   >
                     {{ post.description }}
                   </p>
                 </td>
-                <td class="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                <td class="border-b border-[#999] dark:border-strokedark">
                   <p
-                    class="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success"
+                    class="inline-flex bg-opacity-10 py-1 px-3 text-sm font-medium"
                   >
                     {{ post.created_at }}
                   </p>
